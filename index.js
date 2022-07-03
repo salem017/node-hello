5d1a0e0d99105aa2693f5f6478846fda2ff95305
salem017 <lakhdar.hammache@mines-ales.org>
2022-07-03 17:05:22 +0200

    cors

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+res.setHeader('Access-Control-Allow-Credentials', true);
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
