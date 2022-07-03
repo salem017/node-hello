c250ed5e08b1ef1d893fcbfe49f28b7c0c2a9857
salem017 <lakhdar.hammache@mines-ales.org>
2022-07-03 19:42:19 +0200

    cors

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
+var cors = require('cors');
 const express = require('express');
 const app = express();
+app.use(cors());
 const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader(
+          'Access-Control-Allow-Methods',
+          'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+        );
+        res.setHeader(
+          'Access-Control-Allow-Headers',
+          'X-Requested-With,content-type',
+        );
   app.get('/site', (req, res) => {
       res.setHeader('Access-Control-Allow-Origin', '*');
       res.setHeader(
