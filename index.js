9e97004641aff7dda062e725d2f4b0cf75e5d921
salem017 <lakhdar.hammache@mines-ales.org>
2022-07-03 19:52:45 +0200

    cors

--- index.js
+++ index.js
@@ -1,37 +1,24 @@
-const http = require('http');
-const port = process.env.PORT || 3000;
 var cors = require('cors');
 const express = require('express');
 const app = express();
+const port = 3000;
 app.use(cors());
-const server = http.createServer((req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader(
-          'Access-Control-Allow-Methods',
-          'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-        );
-        res.setHeader(
-          'Access-Control-Allow-Headers',
-          'X-Requested-With,content-type',
-        );
+
   app.get('/site', (req, res) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader(
-        'Access-Control-Allow-Methods',
-        'GET, POST, OPTIONS, PUT, PATCH, DELETE',
-      );
-      res.setHeader(
-        'Access-Control-Allow-Headers',
-        'X-Requested-With,content-type',
-      );
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-Requested-With,content-type',
+    );
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     console.log(req.query.sitetocheck == 'www.zone-annuaire.tel');
     res.send(req.query.sitetocheck == 'www.zone-annuaire.tel');
   });
-  
-});
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
